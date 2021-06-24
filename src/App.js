dd808fb6eb9b652809d0ee712c53b5d48c40714e
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2021-06-24 20:37:39 -0300

    delete SpinnerComponent de la rama equivocada

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
      
       <header className="App-header">
        <img src={logo} className="App-logo" alt="logo" />

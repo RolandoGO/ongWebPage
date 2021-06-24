277b686f25c77a0a5b336010d772daed54070c54
barbiwonderland <barbiiee.8@gmail.com>
2021-06-23 21:44:11 -0300

    Screens folder, main routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Screens/Home";
+import Nosotros from "./Screens/Nosotros";
+import Novedades from "./Screens/Novedades";
+import Actividades from "./Screens/Actividades";
+import Contacto from "./Screens/Contacto";
+import Contribuye from "./Screens/Contribuye";
+import Testimonios from "./Screens/Testimonios";
 
 function App() {
   return (
-    <div className="App">
-      APP
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Nosotros} />
+        <Route exact path="/" component={Actividades} />
+        <Route exact path="/" component={Novedades} />
+        <Route exact path="/" component={Testimonios} />
+        <Route exact path="/" component={Contacto} />
+        <Route exact path="/" component={Contribuye} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Screens/Testimonios.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Testimonios() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Testimonios

74a347191be02c56de9cabf229a92759b6f6bc75
barbiwonderland <barbiiee.8@gmail.com>
2021-06-25 20:48:16 -0300

    creation of alerts

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import SweetAlert from 'sweetalert2-react';
+const Swal = require("sweetalert2");
 
-function Alert() {
-  return (
-    <div>
-     
-    </div>
-  )
+function AlertMessage(type, message) {
+  if (type === "error") {
+    Swal.fire("Algo salio mal", message, "error");
+  } else if (("", type === "success")) {
+    Swal.fire("", message, "success");
+  } else if (type === "info") {
+    Swal.fire("", message, "info");
+  }
 }
 
-export default Alert
-
-
+export default AlertMessage;

313321c0ec19ba438440ef2ccd76205e941d678c
Martin Milani <martin.milani.mdq@gmail.com>
2021-06-25 21:38:47 -0300

    Add HeaderBackOffice and WellcomeMsg components to Dashboard view

---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeaderBackOffice } from "../components/HeaderBackOffice";
+import WellcomeMsg from "../components/WellcomeMsg";
+
+function Dashboard() {
+  return (
+    <>
+      <HeaderBackOffice />
+      <WellcomeMsg />
+    </>
+  );
+}
+
+export default Dashboard;

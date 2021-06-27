fb80616270764877c2ba554af4dccf8ef7f54b6f
XploitXploit <francisco.pellerano1992@gmail.com>
2021-06-26 21:39:11 -0300

    agregue useEffect y exporte el componente

--- src/components/apiService.js
+++ src/components/apiService.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const userToken = localStorage.getItem("token")
-	? localStorage.getItem("token")
-	: "";
+const userToken = '';
 
-const instance = axios.create({
+useEffect(() => {
+	if (localStorage.getItem("token")) {
+		userToken=localStorage.getItem("token")
+	}
+}, [localStorage.getItem("token")])
+
+  
+	
+export const  instance = axios.create({
 	baseURL: "http://url.dummy.org/",
 	headers: { Authorization: "Bearer " + { userToken } },
 });

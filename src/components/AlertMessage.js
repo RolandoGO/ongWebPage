9bf715759a0de8e46d478c81e4f8cd75dd3d42aa
Nico Vazquez <nicov@Nicos-MacBook-Pro.local>
2021-06-28 00:12:46 -0300

    Delete files with duplicate components. Refactor AlertMessage

--- src/components/AlertMessage.js
+++ src/components/AlertMessage.js
@@ -1,13 +1,7 @@
 const Swal = require("sweetalert2");
 
-function AlertMessage(type, message) {
-  if (type === "error") {
-    Swal.fire("Algo salio mal", message, "error");
-  } else if (("", type === "success")) {
-    Swal.fire("", message, "success");
-  } else if (type === "info") {
-    Swal.fire("", message, "info");
-  }
+function AlertMessage(type="info", title="", message="") {
+  Swal.fire(title, message, type);
 }
 
 export default AlertMessage;

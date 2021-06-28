af8515a7e8c38c33fb60facd09e05bbdcdca56ad
Lucas Livchits <livchitsl@gmail.com>
2021-06-28 16:35:02 -0300

    Add an onClick call to show confirmation message
    If the OK button in the message is clicked,
    a method to delete a piece of news is called.
    This method (deleteNews) receive the piece of
    news id and has not been written.

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,17 @@
 import * as React from 'react'
+import AlertMessage from './AlertMessage';
 
 function NewsList() {
+  const handleDelete = (id) => {
+    AlertMessage('warning', 'Está a punto de eliminar una novedad',
+      'Una vez que lo haga no podrá deshacerlo').then(({ value }) => {
+        if (value) {
+          console.log('Novedad eliminada')
+          //deleteNews(id) -> this method should make a DELETE request to de API
+        }
+      })
+  }
+
   const rows = fakeData.map(({ id, name, image, createdAt }) =>
     <tr key={id}>
       <td>{name}</td>
@@ -8,7 +19,7 @@ function NewsList() {
       <td>{createdAt}</td>
       <td>
         <div className="btn-group d-flex justify-content-center" role="group" aria-labelledby="actions">
-          <button type="button" className="btn flex-grow-0 btn-danger">Eliminar</button>
+          <button type="button" className="btn flex-grow-0 btn-danger" onClick={() => handleDelete(id)}>Eliminar</button>
           <button type="button" className="btn flex-grow-0 btn-outline-primary border-start-0">Editar</button>
         </div>
       </td>

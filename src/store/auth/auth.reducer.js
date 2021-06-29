ad1e01b1631a4d9ad8715ebdafa574b52e37cb8a
barbiwonderland <barbiiee.8@gmail.com>
2021-06-29 12:31:49 -0300

    test

--- src/store/auth/auth.reducer.js
+++ src/store/auth/auth.reducer.js
@@ -1,3 +1,23 @@
+import { login } from "../../services/authService"
+
+//accion
+const USER_LOGIN_SUCCESS = USER_LOGIN_SUCCESS
+const USER_LOGIN_REQUEST = USER_LOGIN_REQUEST
+const USER_LOGIN_FAILURE=USER_LOGIN_FAILURE
+//creador de accion, acá habría que conectar con la api?
+export const userLoginSuccess=(token)=>{
+return{
+  type:USER_LOGIN_SUCCESS,
+  payload:token
+}
+}
+// const fetchApi=(email,password)=>{
+// return(dispatch)=>{
+//   dispatch(userLoginSuccess())
+//   login()
+// }
+// }
+
 const initialState = {
     user:"",
     password:"",
@@ -6,6 +26,13 @@ const initialState = {
 };
 
 function AuthReducer(state = initialState, action) {
+  switch(action.type){
+    case USER_LOGIN_SUCCESS:{
+      return{
+
+      }
+    }
+  }
   return state;
 }
 

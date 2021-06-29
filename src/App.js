073b081a967e4620428f5686bb221fc682457849
Lucas Livchits <livchitsl@gmail.com>
2021-06-29 19:06:52 -0300

    Run eslint for all files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./screens/Home";
-import Dashboard from "./screens/Dashboard"
-import BackofficeCategories from "./screens/BackofficeCategories"
+import Dashboard from "./screens/Dashboard";
+import BackofficeCategories from "./screens/BackofficeCategories";
 import { ActivitiesScreen } from "./Screens/ActivitiesScreen";
 
 // import Nosotros from "./screens/Nosotros";
@@ -19,13 +19,19 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/backoffice" component={Dashboard} />
-        <Route exact path="/backoffice/categories" component={BackofficeCategories} />
-        <Route exact path="/backoffice/activities" component={ActivitiesScreen} />
+        <Route
+          exact
+          path="/backoffice/categories"
+          component={BackofficeCategories}
+        />
+        <Route
+          exact
+          path="/backoffice/activities"
+          component={ActivitiesScreen}
+        />
       </Switch>
     </BrowserRouter>
-
   );
 };
 
 export default App;
-

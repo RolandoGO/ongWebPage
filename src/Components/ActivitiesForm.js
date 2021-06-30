1af7ffab86deafbd09fb6687ef8ed65e21061c3f
barbiwonderland <barbiiee.8@gmail.com>
2021-06-29 21:30:25 -0300

    adding ckEditor

--- src/Components/ActivitiesForm.js
+++ src/Components/ActivitiesForm.js
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export const ActivitiesForm = () => {
+  const [description, setDescription] = useState(null);
   return (
     <React.Fragment>
       <div className="container">
         <div className="row d-flex justify-content-center">
-          <div className="col-10 ">
+          <div className="col-8 border p-5 ">
             <form>
-              <div class="form-group my-2">
-                <label for="name">Nombre</label>
-                <input type="name" class="form-control" id="name" />
+              <div className="form-group my-2">
+                <label forhtml="name" className="my-2">
+                  Nombre
+                </label>
+                <input type="name" className="form-control" id="name" />
               </div>
-              <div class="form-group my-2">
-                <label for="image">Imagen:</label>
+              <div className="form-group my-2">
+                <label forhtml="image" className="my-2">
+                  Imagen:
+                </label>
                 <input
                   type="file"
-                  class="form-control-file form-control"
+                  className="form-control-file form-control"
                   id="exampleFormControlFile1"
                 />
               </div>
-              <div class="form-group my-2">
-                <label for="description">Descripción</label>
-                <input
-                  type="description"
-                  class="form-control"
-                  id="description"
+              <div className="form-group my-2">
+                <label forhtml="description" className="my-2">
+                  Descripción
+                </label>
+                <CKEditor
+                  editor={ClassicEditor}
+                  data="Escribe aqui la descripción.."
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    setDescription(data)
+                    console.log(description,"descri")
+                  }}
                 />
               </div>
               <div className="text-center">
-                <button type="submit" class="btn btn-primary">
+                <button type="submit" className="btn btn-primary">
                   Enviar
                 </button>
               </div>

e1f895ca61023fa627b0b6b24504ba40ae044d7b
barbiwonderland <barbiiee.8@gmail.com>
2021-06-30 19:50:32 -0300

    adding validation

--- src/Components/ActivitiesForm.js
+++ src/Components/ActivitiesForm.js
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+import { BsExclamationCircleFill } from "react-icons/bs";
 
-export const ActivitiesForm = ({activity={
-  name:"Pepe",
-  image:"Juana",
-  description:"Maria",
-}}) => {
+export const ActivitiesForm = ({
+  activity = {
+    name: "Pepe",
+    image: "Juana",
+    description: "Luis",
+  },
+}) => {
+  const inputHandler = (event, editor) => {
+    formik.setFieldValue("signator_text", editor.getData());
+  };
   const formik = useFormik({
-    initialValues: 
-     activity
-    ,
+    initialValues: activity,
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Campo Obligatorio"),
+      image: Yup.string().required("Campo Obligatorio"),
+      description: Yup.string()
+        .min(10, "Must be 10 characters or more")
+        .required("Campo Obligatorio"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
-
-
   return (
     <React.Fragment>
       <div className="container">
@@ -31,22 +43,37 @@ export const ActivitiesForm = ({activity={
                 <input
                   type="name"
                   className="form-control"
-                  id="name"
-                  value={formik.values.name}
+                  name="name"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
                 />
+                {formik.touched.name && formik.errors.name ? (
+                  <div className="border  border-danger rounded mt-3 text-danger p-1 align-items-center d-flex justify-content-center">
+                    {formik.errors.name}
+                    <BsExclamationCircleFill className="mx-2" />
+                  </div>
+                ) : null}
               </div>
               <div className="form-group my-2">
                 <label forhtml="image" className="my-2 mx-2">
                   Imagen url:
                 </label>
                 <input
+                  {...formik.getFieldProps("image")}
                   type="name"
-                  className="form-control"
-                  id="image"
-                  value={formik.values.image}
+                  className="form-control "
+                  name="image"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.image}
                 />
+                {formik.touched.image && formik.errors.image ? (
+                  <div className="border  border-danger rounded mt-3 text-danger p-1 align-items-center d-flex justify-content-center">
+                    {formik.errors.image}
+                    <BsExclamationCircleFill className="mx-2" />
+                  </div>
+                ) : null}
               </div>
               <div className="form-group my-2">
                 <label forhtml="description" className="my-2">
@@ -57,11 +84,15 @@ export const ActivitiesForm = ({activity={
                   value={formik.values.description}
                   id="description"
                   editor={ClassicEditor}
-                  data= {formik.values.description}
-                  onChange={(event, editor) => {
-                    const data = editor.getData();
-                  }}
+                  data={formik.values.description}
+                  onChange={inputHandler}
                 />
+                {formik.touched.description && formik.errors.description ? (
+                  <div className="border rounded mt-3 text-danger p-1 align-items-center d-flex justify-content-center">
+                    {formik.errors.description}
+                    <BsExclamationCircleFill className="mx-2" />
+                  </div>
+                ) : null}
               </div>
               <div className="text-center">
                 <button type="submit" className="btn btn-primary">

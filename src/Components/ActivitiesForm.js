9be7ee28ca62e13a9116d3f0f8a6880f2cf7965e
barbiwonderland <barbiiee.8@gmail.com>
2021-06-30 10:58:58 -0300

    change url image and initial values

--- src/Components/ActivitiesForm.js
+++ src/Components/ActivitiesForm.js
@@ -1,29 +1,51 @@
 import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useFormik } from "formik";
 
-export const ActivitiesForm = () => {
-  const [description, setDescription] = useState(null);
+export const ActivitiesForm = ({activity={
+  name:"Pepe",
+  image:"Juana",
+  description:"Maria",
+}}) => {
+  const formik = useFormik({
+    initialValues: 
+     activity
+    ,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+
   return (
     <React.Fragment>
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="col-8 border p-5 ">
-            <form>
+            <form onSubmit={formik.handleSubmit}>
               <div className="form-group my-2">
                 <label forhtml="name" className="my-2">
                   Nombre
                 </label>
-                <input type="name" className="form-control" id="name" />
+                <input
+                  type="name"
+                  className="form-control"
+                  id="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                />
               </div>
               <div className="form-group my-2">
-                <label forhtml="image" className="my-2">
-                  Imagen:
+                <label forhtml="image" className="my-2 mx-2">
+                  Imagen url:
                 </label>
                 <input
-                  type="file"
-                  className="form-control-file form-control"
-                  id="exampleFormControlFile1"
+                  type="name"
+                  className="form-control"
+                  id="image"
+                  value={formik.values.image}
+                  onChange={formik.handleChange}
                 />
               </div>
               <div className="form-group my-2">
@@ -31,12 +53,13 @@ export const ActivitiesForm = () => {
                   Descripción
                 </label>
                 <CKEditor
+                  onChange={formik.handleChange}
+                  value={formik.values.description}
+                  id="description"
                   editor={ClassicEditor}
-                  data="Escribe aqui la descripción.."
+                  data= {formik.values.description}
                   onChange={(event, editor) => {
                     const data = editor.getData();
-                    setDescription(data)
-                    console.log(description,"descri")
                   }}
                 />
               </div>

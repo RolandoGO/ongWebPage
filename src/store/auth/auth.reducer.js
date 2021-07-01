6c33a3f49fc590f972c20beb75546bed553134af
barbiwonderland <barbiiee.8@gmail.com>
2021-07-01 01:32:47 -0300

    modification of reducer and action creator

--- src/store/auth/auth.reducer.js
+++ src/store/auth/auth.reducer.js
@@ -1,39 +1,32 @@
-import { login } from "../../services/authService"
+import { login } from "../../services/authService";
 
-//accion
-const USER_LOGIN_SUCCESS = USER_LOGIN_SUCCESS
-const USER_LOGIN_REQUEST = USER_LOGIN_REQUEST
-const USER_LOGIN_FAILURE=USER_LOGIN_FAILURE
-//creador de accion, acá habría que conectar con la api?
-export const userLoginSuccess=(token)=>{
-return{
-  type:USER_LOGIN_SUCCESS,
-  payload:token
-}
-}
-// const fetchApi=(email,password)=>{
-// return(dispatch)=>{
-//   dispatch(userLoginSuccess())
-//   login()
-// }
-// }
-
-const initialState = {
-    user:"",
-    password:"",
-    email:"",
-    token:"",
+//actions
+const USER_LOGIN_SUCCESS = USER_LOGIN_SUCCESS;
+//action creator
+export const userLoginSuccess = (email, password) => {
+  const { data } = login(email, password);
+  return {
+    type: USER_LOGIN_SUCCESS,
+    payload: data,
+  };
 };
 
+//Reducer
+const initialState = {
+  user: "",
+  password: "",
+  email: "",
+  token: "",
+};
 function AuthReducer(state = initialState, action) {
-  switch(action.type){
-    case USER_LOGIN_SUCCESS:{
-      return{
-
-      }
+  switch (action.type) {
+    case USER_LOGIN_SUCCESS: {
+      return {
+        dataUser: action.payload,
+      };
     }
   }
   return state;
 }
 
-export default AuthReducer
+export default AuthReducer;

22ec6ce877732f6e80255749c539b706e6524df3
Lucas Livchits <livchitsl@gmail.com>
2021-07-03 12:47:18 -0300

    Change functions to returns only the axios call

--- src/services/activitiesService.js
+++ src/services/activitiesService.js
@@ -2,31 +2,15 @@ import axiosInstance from "./apiService";
 
 const ACTIVITIES_PATH = "/activities";
 
-export async function listActivities() {
-  const { success, data, message } = await axiosInstance.get(ACTIVITIES_PATH);
-  return { success, data, message };
-}
+export const listActivities = () => axiosInstance.get(ACTIVITIES_PATH);
 
-export async function createActivity({ name, description, image }) {
-  const { success, data, message } = await axiosInstance.post(ACTIVITIES_PATH, {
-    name,
-    description,
-    image,
-  });
-  return { success, data, message };
-}
+export const createActivity = (activityData) => axiosInstance.post(ACTIVITIES_PATH, activityData);
 
-export async function editActivity({ id, name, description, image }) {
-  const { success, data, message } = await axiosInstance.put(
-    `${ACTIVITIES_PATH}/${id}`,
-    { name, description, image }
-  );
-  return { success, data, message };
-}
+export const editActivity = ({ id, name, description, image }) => axiosInstance.put(
+  `${ACTIVITIES_PATH}/${id}`,
+  { name, description, image }
+);
 
-export async function deleteActivity(id) {
-  const { success, data, message } = await axiosInstance.delete(
-    `${ACTIVITIES_PATH}/${id}`
-  );
-  return { success, data, message };
-}
+export const deleteActivity = (id) => axiosInstance.delete(
+  `${ACTIVITIES_PATH}/${id}`
+);

0a9875a81dab230a8df8bc5768abcf84c3beb068
Lucas Livchits <livchitsl@gmail.com>
2021-07-02 21:38:23 -0300

    Add async thunk for create new activity and its extra reducers

--- src/store/activities/activitiesReducer.js
+++ src/store/activities/activitiesReducer.js
@@ -1,11 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { listActivities } from "../../services/activitiesService";
+import {
+  listActivities,
+  createActivity,
+} from "../../services/activitiesService";
 
 const initialState = { activities: [], status: "idle", error: null };
 
 export const fetchActivities = createAsyncThunk(
   "activities/fetchActivities",
   listActivities
+);
+
+export const postNewActivity = createAsyncThunk(
+  "activities/createActivity",
+  async (newActivity) => await createActivity(newActivity)
 );
 
 const activitiesSlice = createSlice({
@@ -21,6 +29,17 @@ const activitiesSlice = createSlice({
       state.activities = state.activities.concat(action.payload);
     },
     [fetchActivities.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    },
+    [postNewActivity.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [postNewActivity.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state = state.activities.push(action.payload);
+    },
+    [postNewActivity.rejected]: (state, action) => {
       state.status = "failed";
       state.error = action.error.message;
     },

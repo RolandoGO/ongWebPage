24ca9afdbe8f2c472ecf16deef0fda39bfaf40ad
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-04 18:13:13 -0300

    Add postTestimonials function

--- src/Components/testimonials.js
+++ src/Components/testimonials.js
@@ -1,3 +1,11 @@
 import axiosInstance from "./apiService";
 
 export const getTestimonials = () => axiosInstance.get("/testimonials");
+
+export const postTestimonials = (testimonialsData) =>
+  axiosInstance.post("/testimonials", {
+    id: 0,
+    name: testimonialsData.name,
+    image: testimonialsData.image,
+    description: testimonialsData.description,
+  });

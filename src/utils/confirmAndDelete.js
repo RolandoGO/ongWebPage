4136afb989133a22274e5f3a5e624744697e8154
Lucas Livchits <livchitsl@gmail.com>
2021-07-05 15:44:56 -0300

    Add confirm and delete functionality to categories list

--- src/utils/confirmAndDelete.js
+++ src/utils/confirmAndDelete.js
@@ -2,30 +2,33 @@ import AlertMessage from "../components/AlertMessage";
 
 const contentConfiguration = {
   news: {
-    titleText: 'una novedad',
+    titleText: "una novedad",
     onConfirmation: (id) => {
       console.log(`Novedad eliminada con id ${id}`);
       // deleteNews(id) -> this method should make a DELETE request to de API
-    }
+    },
   },
-  categoty: {
-    titleText: 'una categoría',
+  category: {
+    titleText: "una categoría",
     onConfirmation: (id) => {
       console.log(`Categoría eliminada con id ${id}`);
       // deleteCategory(id) -> this method should make a DELETE request to de API
-    }
-  }
-}
+    },
+  },
+};
 
 function confirmAndDelete(id, contentType) {
   const { titleText, onConfirmation } = contentConfiguration[contentType];
 
-  AlertMessage('warning', `Está a punto de eliminar ${titleText}`,
-    'Una vez que lo haga no podrá deshacerlo').then(({ value }) => {
-      if (value) {
-        onConfirmation(id);
-      }
-    })
+  AlertMessage(
+    "warning",
+    `Está a punto de eliminar ${titleText}`,
+    "Una vez que lo haga no podrá deshacerlo"
+  ).then(({ value }) => {
+    if (value) {
+      onConfirmation(id);
+    }
+  });
 }
 
 export default confirmAndDelete;

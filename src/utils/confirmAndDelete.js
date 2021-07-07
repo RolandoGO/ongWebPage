7bbf40f69cf0efe727da0465f21350bf3b956928
barbiwonderland <barbiiee.8@gmail.com>
2021-07-07 20:13:15 -0300

    adding slides

--- src/utils/confirmAndDelete.js
+++ src/utils/confirmAndDelete.js
@@ -1,38 +1,47 @@
-import AlertMessage from "../components/AlertMessage";
-
+import AlertMessage from "../Components/AlertMessage"
 const contentConfiguration = {
   news: {
-    titleText: 'una novedad',
+    titleText: "una novedad",
     onConfirmation: (id) => {
       console.log(`Novedad eliminada con id ${id}`);
       // deleteNews(id) -> this method should make a DELETE request to de API
-    }
+    },
   },
   categoty: {
-    titleText: 'una categoría',
+    titleText: "una categoría",
     onConfirmation: (id) => {
       console.log(`Categoría eliminada con id ${id}`);
       // deleteCategory(id) -> this method should make a DELETE request to de API
-    }
+    },
   },
   activity: {
-    titleText: 'una actividad',
+    titleText: "una actividad",
     onConfirmation: (id) => {
       console.log(`actividad eliminada con id ${id}`);
       // deleteActivity(id) -> this method should make a DELETE request to de API
-    }
-  }
-}
+    },
+  },
+  Slides: {
+    titleText: "una Slide",
+    onConfirmation: (id) => {
+      console.log(`actividad eliminada con id ${id}`);
+      // deleteActivity(id) -> this method should make a DELETE request to de API
+    },
+  },
+};
 
 function confirmAndDelete(id, contentType) {
   const { titleText, onConfirmation } = contentConfiguration[contentType];
 
-  AlertMessage('warning', `Está a punto de eliminar ${titleText}`,
-    'Una vez que lo haga no podrá deshacerlo').then(({ value }) => {
-      if (value) {
-        onConfirmation(id);
-      }
-    })
+  AlertMessage(
+    "warning",
+    `Está a punto de eliminar ${titleText}`,
+    "Una vez que lo haga no podrá deshacerlo"
+  ).then(({ value }) => {
+    if (value) {
+      onConfirmation(id);
+    }
+  });
 }
 
 export default confirmAndDelete;

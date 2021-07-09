f3b352f983ef563e57062e8ab8f4407930f50b0f
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-09 16:56:54 -0300

    Update AlertMessage with Barbara's version

--- src/components/AlertMessage.js
+++ src/components/AlertMessage.js
@@ -1,7 +1,23 @@
 const Swal = require("sweetalert2");
 
-function AlertMessage(type = "info", title = "", message = "") {
-  return Swal.fire(title, message, type);
+function AlertMessage(
+  type = "info",
+  title = "",
+  text = "",
+  confirmButtonText = "",
+  confirmButtonColor = ""
+) {
+  return Swal.fire({
+    title: title,
+    type: type,
+    text: text,
+    showCancelButton: true,
+    // Button to cancel the event
+    cancelButtonText: "Cancelar",
+    confirmButtonColor: confirmButtonColor,
+    // Can be replaced for "SAVE","OK","CANCEL"
+    confirmButtonText: confirmButtonText,
+  });
 }
 
 export default AlertMessage;

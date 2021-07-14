3f2a99ef2c721d290f82eaa0965391c4046f076c
barbiwonderland <barbiiee.8@gmail.com>
2021-07-14 00:29:41 -0300

    modification of fake data

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -3,29 +3,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "react-bootstrap";
 import "./Slider.css";
 
-const FakeInfo = [{
-  "title": "Vagram",
-  "image": "http://dummyimage.com/203x100.png/cc0000/ffffff",
-  "description": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus."
-}, {
-  "title": "Cardguard",
-  "image": "http://dummyimage.com/191x100.png/ff4444/ffffff",
-  "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque."
-}, {
-  "title": "Veribet",
-  "image": "http://dummyimage.com/112x100.png/ff4444/ffffff",
-  "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus."
-}, {
-  "title": "Rank",
-  "image": "http://dummyimage.com/209x100.png/ff4444/ffffff",
-  "description": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum."
-}, {
-  "title": "Tempsoft",
-  "image": "http://dummyimage.com/177x100.png/ff4444/ffffff",
-  "description": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum."
-}]
-
-
+const FakeInfo = [
+  {
+    title: "Vagram",
+    image:
+      "https://i.pinimg.com/originals/52/b6/e2/52b6e237bf1155a8109ead35b2b808d5.jpg",
+    description:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  },
+  {
+    title: "Cardguard",
+    image:
+      "https://www.xtrafondos.com/descargar.php?id=5846&resolucion=3840x2160",
+    description:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  },
+  {
+    title: "Veribet",
+    image:
+      "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+    description:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  },
+  {
+    title: "Rank",
+    image:
+      "https://i.pinimg.com/originals/51/5b/13/515b135f9ed38bb7e2b5856b7192f674.jpg",
+    description:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  },
+  {
+    title: "Tempsoft",
+    image: "https://fondosmil.com/fondo/2292.jpg",
+    description:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  },
+];
 
 export const Slider = () => {
   const Data = useMemo(() => {
@@ -33,10 +46,10 @@ export const Slider = () => {
       FakeInfo &&
       FakeInfo.map((x, i) => {
         return (
-          <Carousel.Item key={i} interval={1000} className="CarouselItem">
-            <img className="d-block w-100"  src={x.image} alt={x.title} />
+          <Carousel.Item key={i} interval={1500} className="CarouselItem">
+            <img className="d-block " src={x.image} alt={x.title} />
             <Carousel.Caption>
-              <h3 className="titles">{x.title}</h3>
+              <h3 className="titles ">{x.title}</h3>
               <p className="subtitles">{x.description}</p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -46,7 +59,7 @@ export const Slider = () => {
   }, []);
   return (
     <React.Fragment>
-      <Carousel  fade>{Data}</Carousel>
+      <Carousel fade>{Data}</Carousel>
     </React.Fragment>
   );
 };

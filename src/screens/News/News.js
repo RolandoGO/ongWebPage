d6ff544f3d8cef838c7ad2c1af207d8f3343d7d3
barbiwonderland <barbiiee.8@gmail.com>
2021-07-15 10:49:38 -0300

    adding the folder News to the proyect with the index and related components

---
+++ src/screens/News/News.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+// import { editNews, deleteNews, getNews, createNews} from '../services/newsService';
+// import { HeaderBackOffice } from "../components/HeaderBackOffice";
+// import NewsList from "../components/NewsList";
+
+function News() {  
+
+  return (
+    <>
+    <h1>Hello</h1>
+      {/* <HeaderBackOffice createNews={createNews} /> */}
+      {/* <NewsList editNews={editNews} deleteNews={deleteNews} getNews={getNews}/> */}
+    </>
+  );
+}
+
+export default News;
+
+
+
+
+
+

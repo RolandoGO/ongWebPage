28ed126ddbd58a589d4627d7084853bb771dffa0
barbiwonderland <barbiiee.8@gmail.com>
2021-07-15 00:01:39 -0300

    styles

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -1,64 +1,59 @@
 import React, { useMemo } from "react";
 import "./Members.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const fakeData = [
   {
     name: "Voltsillam",
     image: "images/members/CeciliaMendez.jpeg",
-    description:
-      "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+    description: "Maecenas leo odio, condimentum id",
     facebookUrl: "Cis Woman",
-    instagramUrl: "Neither",
+    linkedinUrl: "Neither",
   },
   {
     name: "Bamity",
     image: "images/members/Marco Fernandez.jpg",
-    description:
-      "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+    description: "Quisque porta volutpat erat. Quisque erat eros, viverra eget",
     facebookUrl: "Transgender Man",
-    instagramUrl: "Bigender",
+    linkedinUrl: "Bigender",
   },
   {
     name: "Flowdesk",
     image: "images/members/María Garcia.jpg",
-    description:
-      "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. ",
+    description: "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum. ",
     facebookUrl: "Transgender Female",
-    instagramUrl: "Transgender Person",
+    linkedinUrl: "Transgender Person",
   },
   {
     name: "Tempsoft",
     image: "images/members/María Irola.jpg",
     description:
-      "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
+      "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi.",
     facebookUrl: "Female to Male",
-    instagramUrl: "Trans",
+    linkedinUrl: "Trans",
   },
   {
     name: "Sub-Ex",
     image: "images/members/Marita Gomez.jpeg",
-    description:
-      "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+    description: "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum. ",
     facebookUrl: "Transsexual Woman",
-    instagramUrl: "Androgyne",
+    linkedinUrl: "Androgyne",
   },
   {
     name: "Tres-Zap",
     image: "images/members/Miriam Rodriguez.jpg",
-    description:
-      "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+    description: "Quisque id justo sit amet sapien dignissim vestibulum. ",
     facebookUrl: "Transgender Man",
-    instagramUrl: "Androgynous",
+    linkedinUrl: "Androgynous",
   },
   {
     name: "Zontrax",
     image: "images/members/Rodrigo Fuente.jpg",
     description:
-      "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+      "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.",
     facebookUrl: "Gender Variant",
-    instagramUrl: "Transsexual Female",
+    linkedinUrl: "Transsexual Female",
   },
 ];
 
@@ -67,24 +62,28 @@ export const Members = () => {
     return fakeData.map((x) => {
       return (
         <div className="col-md-3 text-center d-flex">
-          <div className=" card my-2 py-3">
-            <div className="mt-3">
+          <div className=" card my-2 ">
+            <div className="my-3 text-center pb-3 ">
               <img
                 className=" zoom card-img-top mx-auto rounded-circle coverImg sizeImg"
                 src={x.image}
                 alt={x.name}
               />
-              <div className="card-body ">
-                <h5 className="card-title">{x.name}</h5>
-                <p className="card-text">{x.description}</p>
-                <a href={x.facebookUrl} className="btn btn-primary hover">
-                  <FaFacebook />
-                </a>
-
-                <a href={x.instagramUrl} className="btn btn-primary mx-3 hover">
-                  <FaInstagram />
-                </a>
+              <div className="card-body  ">
+                <h5 className=" titles card-title ">{x.name}</h5>
+                <div className="triangulo">
+                  <p className=" card-text subtitles mb-3">{x.description}</p>
+                </div>
               </div>
+            </div>
+            <div className="bottom mb-2">
+              <a href={x.facebookUrl} className="btn btn-primary hover">
+                <FaFacebook />
+              </a>
+
+              <a href={x.linkedinUrl} className="btn btn-primary mx-3 hover">
+                <FaLinkedin />
+              </a>
             </div>
           </div>
         </div>

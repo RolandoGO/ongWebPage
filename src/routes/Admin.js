976bc479c5bcdc60bb15ac0842b9a8368922553c
barbiwonderland <barbiiee.8@gmail.com>
2021-07-15 01:48:01 -0300

    adding the  routes from backoffice

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,34 +1,26 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Dashboard from "../screens/Dashboard"
-import {ActivitiesScreen} from "../screens/ActivitiesScreen"
-import {MembersFormBackOffice} from "../Components/MembersFormBackOffice"
+import Dashboard from "../screens/Dashboard";
+import { ActivitiesScreen } from "../screens/ActivitiesScreen";
+import { MembersFormBackOffice } from "../Components/MembersFormBackOffice";
 import BackOfficeSlide from "../Components/BackOfficeSlides";
-// import {BackofficeCategories} from "../Components/BackofficeCategories";
+import BackofficeCategories from "../Components/BackofficeCategories";
 export const Admin = () => {
   return (
-
-      <Switch>
-          <h1>ADMI</h1>
-        <Route exact path="/backoffice" component={Dashboard} />
-        {/* <Route
-          exact
-          path="/backoffice/categories"
-          component={BackofficeCategories}
-        /> */}
-        <Route
-          exact
-          path="/backoffice/activities"
-          component={ActivitiesScreen}
-        />
-        <Route
-          exact
-          path="/backoffice/organization/edit"
-          component={MembersFormBackOffice}
-        />
-        <Route exact path="/backoffice/slides" component={BackOfficeSlide} />
-        <Route exact path="/backoffice/slides/create" />
-      </Switch>
- 
+    <Switch>
+      <Route exact path="/admin/backoffice" component={Dashboard} />
+      <Route
+        exact
+        path="/admin/backoffice/categories"
+        component={BackofficeCategories}
+      />
+      <Route path="/admin/backoffice/activities" component={ActivitiesScreen} />
+      <Route
+        path="/admin/backoffice/organization/edit"
+        component={MembersFormBackOffice}
+      />
+      <Route path="/admin/backoffice/slides" component={BackOfficeSlide} />
+      <Route path="/admin/backoffice/slides/create" />
+    </Switch>
   );
 };

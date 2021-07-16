a94155cd0c4085a3baa35de7e5c1428d73d2141f
XploitXploit <francisco.pellerano1992@gmail.com>
2021-07-16 10:45:58 -0300

    Refactor code and remove try/catch

--- src/services/homeService.js
+++ src/services/homeService.js
@@ -1,29 +1,9 @@
 import { getTestimonials } from "./testimonials";
 import { getNews } from "./newsService";
-import AlertMessage from "../components/AlertMessage";
 
-export const listNews = () => {
-	try {
-        const listOfNews = getNews().data;
-        return listOfNews
-	} catch (error) {
-		AlertMessage(
-			"info",
-			"Error",
-			`${error.error.message ? error.error.message : error.error}`
-		);
-	}
-};
+export const listNews = () => getNews().data;
 
-export const listTestimonials = () => {
-    try {
-        const listOfTestimonials = getTestimonials().data;
-        return listOfTestimonials
-	} catch (error) {
-		AlertMessage(
-			"info",
-			"Error",
-			`${error.error.message ? error.error.message : error.error}`
-		);
-	}
-}
+export const listTestimonials= () => getTestimonials().data;
+		
+	
+

579d2bcd6b76a5bdd1d96e5d1a7c6f5d3dc62128
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2021-07-16 10:17:30 -0300

    changing the name to ContactData

--- src/Components/ContactData.js
+++ src/Components/ContactData.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export default function ContactDataComponent({data}) {
+export default function ContactData({data}) {
     
     const [contactData, setContactData]=useState()
 

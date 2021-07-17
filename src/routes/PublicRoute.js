67af28cbf572f6b9ac655c23ef58396f045c2960
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-17 20:30:05 -0300

    Add route to match /nosotros in PublicRoute.js

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../screens/Home";
 import News from "../screens/news";
+import About from "../screens/about";
 
 export const PublicRoute = () => {
   return (
     <Switch>
       <Route exact path="/novedades" component={News} />
+      <Route exact path="/nosotros" component={About} />
       <Route path="/" component={Home} />
     </Switch>
   );

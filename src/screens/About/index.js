63cd376fbe2b843842038d6c8602aecbdd664cf1
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-17 17:57:42 -0300

    Add index.js

---
+++ src/screens/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <div className="container">
+      <h1 className="text-center m-2 p-2">Nosotros</h1>
+    </div>
+  );
+}
+
+export default index;

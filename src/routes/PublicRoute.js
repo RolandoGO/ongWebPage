9dd43be1b5a59ce6bc9126d00f74afd259352040
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-18 16:56:23 -0300

    Add route to match /novedades/:id in PublicRoutes.js

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../screens/Home";
 import News from "../screens/news";
+import Details from "../screens/news/details";
 
 export const PublicRoute = () => {
   return (
     <Switch>
       <Route exact path="/novedades" component={News} />
+      <Route exact path="/novedades/:id" component={Details} />
       <Route path="/" component={Home} />
     </Switch>
   );

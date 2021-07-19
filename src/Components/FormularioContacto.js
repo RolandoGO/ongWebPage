ecddb618c4dc7d45b5d4aa49befed9fc338da6c6
Lucas Livchits <livchitsl@gmail.com>
2021-07-19 16:16:25 -0300

    Add a call to postMessage function on submit

--- src/Components/FormularioContacto.js
+++ src/Components/FormularioContacto.js
@@ -2,187 +2,192 @@ import React, { useState } from "react";
 import { Field, Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import AlertMessage from "../components/AlertMessage";
+import { postMessage } from "../services/contactsService";
 
 import "../styles/contactForm.css";
 
 const handleClick = () => {
-	const responseAlert = false;
-	AlertMessage(
-		"info",
-		"Consulta",
-		"Seguro que desea cancelar?",
-		"Aceptar"
-	).then((value) => {
-		if (value) {
-			//Do something
-            console.log(value);
-		} else {
-            console.log(value);
-		}
-	});
+  const responseAlert = false;
+  AlertMessage(
+    "info",
+    "Consulta",
+    "Seguro que desea cancelar?",
+    "Aceptar"
+  ).then((value) => {
+    if (value) {
+      //Do something
+      console.log(value);
+    } else {
+      console.log(value);
+    }
+  });
 };
 
 function FormularioContacto() {
-	const [addData, setData] = useState();
+  const [addData, setData] = useState();
 
-	const phoneRegExp =
-		/^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d][\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$/;
+  const phoneRegExp =
+    /^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d][\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$/;
 
-	const validationSchema = Yup.object({
-		name: Yup.string()
-			.min(3, "Demasiado Corto!")
-			.max(20, "Demasiado Largo!")
-			.required("Requerido!"),
-		email: Yup.string()
-			.email("No es un Email Valido!")
-			.min(12, "Demasiado Corto!")
-			.max(40, "Demasiado Largo!")
-			.required("Requerido!"),
-		phone: Yup.string()
-			.matches(phoneRegExp, "No es un número válido")
-			.max(13, "Demasiado Largo!")
-			.min(8, "Demasiado Corto!!")
-			.required("Required"),
-		message: Yup.string()
-			.min(10, "El mensaje es muy corto")
-			.max(500, "Demasiado Largo!")
-			.required("Requerido!"),
-	});
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, "Demasiado Corto!")
+      .max(20, "Demasiado Largo!")
+      .required("Requerido!"),
+    email: Yup.string()
+      .email("No es un Email Valido!")
+      .min(12, "Demasiado Corto!")
+      .max(40, "Demasiado Largo!")
+      .required("Requerido!"),
+    phone: Yup.string()
+      .matches(phoneRegExp, "No es un número válido")
+      .max(13, "Demasiado Largo!")
+      .min(8, "Demasiado Corto!!")
+      .required("Required"),
+    message: Yup.string()
+      .min(10, "El mensaje es muy corto")
+      .max(500, "Demasiado Largo!")
+      .required("Requerido!"),
+  });
 
-	return (
-		<div className='container-fluid '>
-			<Formik
-				initialValues={{ name: "", email: "", phone: "", message: "" }}
-				onSubmit={(values, actions, errors) => {
-					console.log(values);
-				}}
-				validationSchema={validationSchema}
-				validateOnChange={false}
-				validateOnBlur={false}>
-				{({ handleSubmit, handleChange, values }) => (
-					<Form
-						onSubmit={handleSubmit}
-						className='container py-2  col-*8 col-sm-9 col-md-9 col-lg-9 mb-4'>
-						<h3>Nombre</h3>
-						<div className='row px-2 py-1'>
-							<Field name='name'>
-								{({ field, form: { touched, errors }, meta }) => (
-									<div className='row px-2 py-1'>
-										<input
-											name={field.name}
-											type='text'
-											value={values.name}
-											onChange={handleChange}
-											placeholder='Nombre'></input>
-									</div>
-								)}
-							</Field>
-						</div>
-						<ErrorMessage
-							name='name'
-							render={(msg) => (
-								<div className='alert alert-info' role='alert'>
-									{msg}
-								</div>
-							)}
-						/>
+  return (
+    <div className="container-fluid ">
+      <Formik
+        initialValues={{ name: "", email: "", phone: "", message: "" }}
+        onSubmit={postMessage}
+        validationSchema={validationSchema}
+        validateOnChange={false}
+        validateOnBlur={false}
+      >
+        {({ handleSubmit, handleChange, values }) => (
+          <Form
+            onSubmit={handleSubmit}
+            className="container py-2  col-*8 col-sm-9 col-md-9 col-lg-9 mb-4"
+          >
+            <h3>Nombre</h3>
+            <div className="row px-2 py-1">
+              <Field name="name">
+                {({ field, form: { touched, errors }, meta }) => (
+                  <div className="row px-2 py-1">
+                    <input
+                      name={field.name}
+                      type="text"
+                      value={values.name}
+                      onChange={handleChange}
+                      placeholder="Nombre"
+                    ></input>
+                  </div>
+                )}
+              </Field>
+            </div>
+            <ErrorMessage
+              name="name"
+              render={(msg) => (
+                <div className="alert alert-info" role="alert">
+                  {msg}
+                </div>
+              )}
+            />
 
-						<h3>Email</h3>
-						<div className='row px-2 py-1'>
-							<Field name='email'>
-								{({ field, form: { touched, errors }, meta }) => (
-									<div className='row px-2 py-1'>
-										<input
-											name={field.name}
-											type='text'
-											value={values.email}
-											onChange={handleChange}
-											placeholder='Email'></input>
-									</div>
-								)}
-							</Field>
-						</div>
-						<ErrorMessage
-							className='py-0'
-							name='email'
-							render={(msg) => (
-								<div className='alert alert-info' role='alert'>
-									{msg}
-								</div>
-							)}
-						/>
+            <h3>Email</h3>
+            <div className="row px-2 py-1">
+              <Field name="email">
+                {({ field, form: { touched, errors }, meta }) => (
+                  <div className="row px-2 py-1">
+                    <input
+                      name={field.name}
+                      type="text"
+                      value={values.email}
+                      onChange={handleChange}
+                      placeholder="Email"
+                    ></input>
+                  </div>
+                )}
+              </Field>
+            </div>
+            <ErrorMessage
+              className="py-0"
+              name="email"
+              render={(msg) => (
+                <div className="alert alert-info" role="alert">
+                  {msg}
+                </div>
+              )}
+            />
 
-						<h3>Teléfono</h3>
-						<div className='row px-2 py-1'>
-							<Field name='phone'>
-								{({ field, form: { touched, errors }, meta }) => (
-									<div className='row px-2 py-1'>
-										<input
-											name={field.name}
-											type='text'
-											placeholder='Teléfono'
-											value={values.phone}
-											onChange={handleChange}></input>
-									</div>
-								)}
-							</Field>
-						</div>
+            <h3>Teléfono</h3>
+            <div className="row px-2 py-1">
+              <Field name="phone">
+                {({ field, form: { touched, errors }, meta }) => (
+                  <div className="row px-2 py-1">
+                    <input
+                      name={field.name}
+                      type="text"
+                      placeholder="Teléfono"
+                      value={values.phone}
+                      onChange={handleChange}
+                    ></input>
+                  </div>
+                )}
+              </Field>
+            </div>
 
-						<ErrorMessage
-							name='phone'
-							render={(msg) => (
-								<div className='alert alert-info' role='alert'>
-									{msg}
-								</div>
-							)}
-						/>
+            <ErrorMessage
+              name="phone"
+              render={(msg) => (
+                <div className="alert alert-info" role="alert">
+                  {msg}
+                </div>
+              )}
+            />
 
-						<h3>Mensaje</h3>
-						<div className='row px-2 py-1'>
-							<Field name='message'>
-								{({ field, form: { touched, errors }, meta }) => (
-									<div className='row px-2  py-0.5'>
-										<textarea
-											name={field.name}
-											type='textfield'
-											rows='8'
-											cols='40'
-											value={values.message}
-											onChange={handleChange}
-											className='textarea'
-											placeholder='Ingrese su Mensaje'
-										/>
-									</div>
-								)}
-							</Field>
-						</div>
-						<ErrorMessage
-							name='message'
-							render={(msg) => (
-								<div className='alert alert-info' role='alert'>
-									{msg}
-								</div>
-							)}
-						/>
+            <h3>Mensaje</h3>
+            <div className="row px-2 py-1">
+              <Field name="message">
+                {({ field, form: { touched, errors }, meta }) => (
+                  <div className="row px-2  py-0.5">
+                    <textarea
+                      name={field.name}
+                      type="textfield"
+                      rows="8"
+                      cols="40"
+                      value={values.message}
+                      onChange={handleChange}
+                      className="textarea"
+                      placeholder="Ingrese su Mensaje"
+                    />
+                  </div>
+                )}
+              </Field>
+            </div>
+            <ErrorMessage
+              name="message"
+              render={(msg) => (
+                <div className="alert alert-info" role="alert">
+                  {msg}
+                </div>
+              )}
+            />
 
-						<div className='row py-2'>
-							<div className='col'>
-								<button type='submit' class='btn btn-primary'>
-									Enviar
-								</button>
-								<button
-									type='button'
-									onClick={handleClick}
-									class='btn btn-primary'>
-									Cancelar
-								</button>
-							</div>
-						</div>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            <div className="row py-2">
+              <div className="col">
+                <button type="submit" class="btn btn-primary">
+                  Enviar
+                </button>
+                <button
+                  type="button"
+                  onClick={handleClick}
+                  class="btn btn-primary"
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default FormularioContacto;

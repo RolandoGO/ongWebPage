d4140dbcd9ae42af53343c0e202e59b5ba4104a5
Lucas Livchits <livchitsl@gmail.com>
2021-07-19 19:19:53 -0300

    Add success message and extract logic from render

--- src/Components/FormularioContacto.js
+++ src/Components/FormularioContacto.js
@@ -58,12 +58,26 @@ function FormularioContacto() {
 
       if (success) {
         resetForm();
+        setStatus({ submit: "success" });
       } else {
         throw new Error();
       }
     } catch (error) {
-      setStatus({ submitError: true });
+      setStatus({ submit: "failed" });
     }
+  };
+
+  const postSubmitMessage = {
+    success: (
+      <div class="alert alert-success" role="alert">
+        El mensaje fue enviado con éxito
+      </div>
+    ),
+    failed: (
+      <div class="alert alert-danger" role="alert">
+        Hubo un error al enviar el mensaje
+      </div>
+    ),
   };
 
   return (
@@ -75,134 +89,134 @@ function FormularioContacto() {
         validateOnChange={false}
         validateOnBlur={false}
       >
-        {({ handleChange, values, status }) => (
-          <Form className="container py-2  col-*8 col-sm-9 col-md-9 col-lg-9 mb-4">
-            <h3>Nombre</h3>
-            <div className="row px-2 py-1">
-              <Field name="name">
-                {({ field, form: { touched, errors }, meta }) => (
-                  <div className="row px-2 py-1">
-                    <input
-                      name={field.name}
-                      type="text"
-                      value={values.name}
-                      onChange={handleChange}
-                      placeholder="Nombre"
-                    ></input>
-                  </div>
-                )}
-              </Field>
-            </div>
-            <ErrorMessage
-              name="name"
-              render={(msg) => (
-                <div className="alert alert-info" role="alert">
-                  {msg}
-                </div>
-              )}
-            />
+        {({ handleChange, values, status }) => {
+          const postSubmitMessageToShow = postSubmitMessage[status?.submit];
 
-            <h3>Email</h3>
-            <div className="row px-2 py-1">
-              <Field name="email">
-                {({ field, form: { touched, errors }, meta }) => (
-                  <div className="row px-2 py-1">
-                    <input
-                      name={field.name}
-                      type="text"
-                      value={values.email}
-                      onChange={handleChange}
-                      placeholder="Email"
-                    ></input>
-                  </div>
-                )}
-              </Field>
-            </div>
-            <ErrorMessage
-              className="py-0"
-              name="email"
-              render={(msg) => (
-                <div className="alert alert-info" role="alert">
-                  {msg}
-                </div>
-              )}
-            />
-
-            <h3>Teléfono</h3>
-            <div className="row px-2 py-1">
-              <Field name="phone">
-                {({ field, form: { touched, errors }, meta }) => (
-                  <div className="row px-2 py-1">
-                    <input
-                      name={field.name}
-                      type="text"
-                      placeholder="Teléfono"
-                      value={values.phone}
-                      onChange={handleChange}
-                    ></input>
-                  </div>
-                )}
-              </Field>
-            </div>
-
-            <ErrorMessage
-              name="phone"
-              render={(msg) => (
-                <div className="alert alert-info" role="alert">
-                  {msg}
-                </div>
-              )}
-            />
-
-            <h3>Mensaje</h3>
-            <div className="row px-2 py-1">
-              <Field name="message">
-                {({ field, form: { touched, errors }, meta }) => (
-                  <div className="row px-2  py-0.5">
-                    <textarea
-                      name={field.name}
-                      type="textfield"
-                      rows="8"
-                      cols="40"
-                      value={values.message}
-                      onChange={handleChange}
-                      className="textarea"
-                      placeholder="Ingrese su Mensaje"
-                    />
-                  </div>
-                )}
-              </Field>
-            </div>
-            <ErrorMessage
-              name="message"
-              render={(msg) => (
-                <div className="alert alert-info" role="alert">
-                  {msg}
-                </div>
-              )}
-            />
-
-            <div className="row py-2">
-              <div className="col">
-                <button type="submit" class="btn btn-primary">
-                  Enviar
-                </button>
-                <button
-                  type="button"
-                  onClick={handleClick}
-                  class="btn btn-primary"
-                >
-                  Cancelar
-                </button>
+          return (
+            <Form className="container py-2  col-*8 col-sm-9 col-md-9 col-lg-9 mb-4">
+              <h3>Nombre</h3>
+              <div className="row px-2 py-1">
+                <Field name="name">
+                  {({ field, form: { touched, errors }, meta }) => (
+                    <div className="row px-2 py-1">
+                      <input
+                        name={field.name}
+                        type="text"
+                        value={values.name}
+                        onChange={handleChange}
+                        placeholder="Nombre"
+                      ></input>
+                    </div>
+                  )}
+                </Field>
               </div>
-            </div>
-            {status?.submitError && (
-              <div class="alert alert-danger" role="alert">
-                Hubo un error al enviar el mensaje
+              <ErrorMessage
+                name="name"
+                render={(msg) => (
+                  <div className="alert alert-info" role="alert">
+                    {msg}
+                  </div>
+                )}
+              />
+
+              <h3>Email</h3>
+              <div className="row px-2 py-1">
+                <Field name="email">
+                  {({ field, form: { touched, errors }, meta }) => (
+                    <div className="row px-2 py-1">
+                      <input
+                        name={field.name}
+                        type="text"
+                        value={values.email}
+                        onChange={handleChange}
+                        placeholder="Email"
+                      ></input>
+                    </div>
+                  )}
+                </Field>
               </div>
-            )}
-          </Form>
-        )}
+              <ErrorMessage
+                className="py-0"
+                name="email"
+                render={(msg) => (
+                  <div className="alert alert-info" role="alert">
+                    {msg}
+                  </div>
+                )}
+              />
+
+              <h3>Teléfono</h3>
+              <div className="row px-2 py-1">
+                <Field name="phone">
+                  {({ field, form: { touched, errors }, meta }) => (
+                    <div className="row px-2 py-1">
+                      <input
+                        name={field.name}
+                        type="text"
+                        placeholder="Teléfono"
+                        value={values.phone}
+                        onChange={handleChange}
+                      ></input>
+                    </div>
+                  )}
+                </Field>
+              </div>
+
+              <ErrorMessage
+                name="phone"
+                render={(msg) => (
+                  <div className="alert alert-info" role="alert">
+                    {msg}
+                  </div>
+                )}
+              />
+
+              <h3>Mensaje</h3>
+              <div className="row px-2 py-1">
+                <Field name="message">
+                  {({ field, form: { touched, errors }, meta }) => (
+                    <div className="row px-2  py-0.5">
+                      <textarea
+                        name={field.name}
+                        type="textfield"
+                        rows="8"
+                        cols="40"
+                        value={values.message}
+                        onChange={handleChange}
+                        className="textarea"
+                        placeholder="Ingrese su Mensaje"
+                      />
+                    </div>
+                  )}
+                </Field>
+              </div>
+              <ErrorMessage
+                name="message"
+                render={(msg) => (
+                  <div className="alert alert-info" role="alert">
+                    {msg}
+                  </div>
+                )}
+              />
+
+              <div className="row py-2">
+                <div className="col">
+                  <button type="submit" class="btn btn-primary">
+                    Enviar
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleClick}
+                    class="btn btn-primary"
+                  >
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+              {postSubmitMessageToShow}
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );

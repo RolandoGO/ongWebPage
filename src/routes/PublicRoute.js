70045cd8ff7febb5da755612bac842cdd7467033
Federico <jf.montillo@gmail.com>
2021-07-20 20:23:35 -0300

    create Activities component

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../screens/Home";
 import News from "../screens/News";
 import Contact from "../screens/contact";
+import Activities from "../screens/activities";
 
 export const PublicRoute = () => {
   return (
     <Switch>
       <Route exact path="/contacto" component={Contact} />
       <Route exact path="/novedades" component={News} />
+      <Route exact path="/actividades" component={Activities} />
       <Route path="/" component={Home} />
     </Switch>
   );

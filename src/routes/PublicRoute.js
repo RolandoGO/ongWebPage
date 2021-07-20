6f07f1262162980437f42cf24fac76fa9b0bd6d4
Federico <jf.montillo@gmail.com>
2021-07-20 15:32:49 -0300

    Add animation nav

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "../screens/Home";
-import News from "../screens/news";
+import News from "../screens/News";
 import Contact from "../screens/contact";
+import { AnimatedSwitch } from "react-router-transition";
 
 export const PublicRoute = () => {
   return (
-    <Switch>
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 1 }}
+      atActive={{ opacity: 1 }}
+    >
       <Route exact path="/contacto" component={Contact} />
       <Route exact path="/novedades" component={News} />
       <Route path="/" component={Home} />
-    </Switch>
+    </AnimatedSwitch>
   );
 };

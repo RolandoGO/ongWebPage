204392cd3927b5b775ca8c7cb680836469c7aa03
Lucas Livchits <livchitsl@gmail.com>
2021-07-20 16:01:33 -0300

    Add loading state and show Loader when the state is true

--- src/Components/NewsHome.js
+++ src/Components/NewsHome.js
@@ -1,21 +1,25 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from "react";
 import { getNews } from "../services/newsService";
+import { LoaderSpinner } from "../components/LoaderSpinner";
 
 export const NewsHome = () => {
-
-  const [ news, setNews ] = useState([]);
-  const [ errorNews, seterrorNews ] = useState(null);
+  const [news, setNews] = useState([]);
+  const [errorNews, seterrorNews] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Integration API
   const getNewsNotices = async () => {
     try {
+      setIsLoading(true);
       const resultado = await getNews();
       const { data } = resultado;
       const news = data.data;
       setNews(news);
     } catch (error) {
-      const errorNews = "Upsss... No se pudo cargar el contenido."
+      const errorNews = "Upsss... No se pudo cargar el contenido.";
       seterrorNews(errorNews);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -23,10 +27,9 @@ export const NewsHome = () => {
     getNewsNotices();
   }, []);
 
-
   const memoNews = useMemo(
     () =>
-    news.map((newNotice) => {
+      news.map((newNotice) => {
         return (
           // <NewsHomeCard key={newNotice.id} newNotice={newNotice} /> Place Card component, Ticket 98 in progress
         );
@@ -34,13 +37,23 @@ export const NewsHome = () => {
     [news]
   );
 
-  const memoOrError = !errorNews ? memoNews : <p className="alert alert-danger text-center">{errorNews}</p>;
+  const memoOrError = !errorNews ? (
+    memoNews
+  ) : (
+    <p className="alert alert-danger text-center">{errorNews}</p>
+  );
+
+  if (isLoading) {
+    return (
+      <div className="d-flex justify-content-center h-100 align-items-center">
+        <LoaderSpinner />
+      </div>
+    );
+  }
 
   return (
     <div className="container">
-      <div className="row mt-5">
-          {memoOrError}
-      </div>
+      <div className="row mt-5">{memoOrError}</div>
     </div>
-  )
+  );
 };

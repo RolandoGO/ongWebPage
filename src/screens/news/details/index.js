c0ed6688a13fde9757683575744cbf6fd6fb6687
Lucas Livchits <livchitsl@gmail.com>
2021-07-20 23:04:53 -0300

    Add logic to get news data
    - Add useParams to get the news id.
    - Create a function to get the news data using the corresponding service
    - Add state for the news data and request status
    - Define a dictionary with the content to render for each request status:
      - Loader component while loading
      - Error message if the request fails
      - The news data, with components for title and content, if the request is successful

--- src/screens/news/details/index.js
+++ src/screens/news/details/index.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { LoaderSpinner } from "../../../components/LoaderSpinner";
+import { TitlesComponent } from "../../../Components/TitlesComponent";
+import { getSingleNew } from "../../../services/newsService";
+import NewsWebContent from "../../../Components/NewsWebContent";
 
-function index() {
-  return (
-    <div>
-      <h1>News Details</h1>
-    </div>
-  );
+function Details() {
+  const [{ newsDetails, status }, setNewsDetails] = React.useState({
+    newsDetails: null,
+    status: "loading",
+  });
+
+  const { id } = useParams();
+
+  const getNewsDetails = async (id) => {
+    try {
+      const { data } = await getSingleNew(id); //the method is being created in ticket #119
+      setNewsDetails({ newsDetails: data.data, status: "complete" });
+    } catch (error) {
+      setNewsDetails((state) => ({ ...state, status: "failed" }));
+    }
+  };
+
+  React.useEffect(() => {
+    getNewsDetails(id);
+  }, [id]);
+
+  const contentToRender = {
+    loading: (
+      <div className="d-flex justify-content-center h-100 align-items-center">
+        <LoaderSpinner />
+      </div>
+    ),
+    complete: (
+      <>
+        <TitlesComponent title={newsDetails.name} img={newsDetails.image} />
+        <NewsWebContent data={newsDetails} />
+        {/* the component is being created in ticket #117 */}
+      </>
+    ),
+    failed: (
+      <p className="alert alert-danger text-center">
+        Upsss... No se pudo cargar el contenido.
+      </p>
+    ),
+  };
+
+  return <section className="container">{contentToRender[status]}</section>;
 }
 
-export default index;
+export default Details;

cc23c99043a0250d2b9b1bd98869ba41359eeb91
Lucas Livchits <livchitsl@gmail.com>
2021-07-21 16:43:46 -0300

    Add route for activity details

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -13,6 +13,14 @@ export const PublicRoute = () => {
       <Route exact path="/novedades" component={News} />
       <Route exact path="/nosotros" component={About} />
       <Route exact path="/novedades/:id" component={Details} />
+      <Route
+        exact
+        path="/actividad/:id"
+        render={({ match }) => {
+          const { id } = match.params;
+          return <Activity id={id} />;
+        }}
+      />
       <Route path="/" component={Home} />
     </Switch>
   );

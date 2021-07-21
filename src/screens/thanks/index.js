2d622751ff252423f1eda14701c29dd2d58d114c
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-21 19:21:09 -0300

    Add gracias screen and integrate TitleComponent

---
+++ src/screens/thanks/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TitlesComponent } from "../../components/TitlesComponent";
+
+function index() {
+  return (
+    <div>
+      <TitlesComponent title={"Gracias"} />
+    </div>
+  );
+}
+
+export default index;

3090d7ecfb34cc544ee574f116eaef8345c75d13
Nico Vazquez <nicov@Nicos-MacBook-Pro.local>
2021-07-22 17:21:16 -0300

    fixed header on merge

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Route, BrowserRouter } from "react-router-dom";
 import { PublicRoute } from "./routes/PublicRoute";
 import { Admin } from "./routes/Admin";
 import { AnimatedSwitch } from "react-router-transition";
-import FooterWeb from "./components/FooterWeb";
-
-import HeaderWeb from "./components/HeaderWeb";
 
 const App = () => {
   //Check if a token exists in localstorage
@@ -17,7 +14,6 @@ const App = () => {
 
   return (
     <BrowserRouter>
-     <HeaderWeb isLogIn={false} />
       <AnimatedSwitch
         atEnter={{ opacity: 0 }}
         atLeave={{ opacity: 1 }}
@@ -27,7 +23,6 @@ const App = () => {
         <Route path="/backoffice" component={Admin} />
         <Route path="/" component={PublicRoute} />
       </AnimatedSwitch>
-      <FooterWeb />
     </BrowserRouter>
   );
 };
--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -4,17 +4,23 @@ import Home from "../screens/Home";
 import News from "../screens/News";
 import Contact from "../screens/contact";
 import { AnimatedSwitch } from "react-router-transition";
+import FooterWeb from "../components/FooterWeb";
 
+import HeaderWeb from "../components/HeaderWeb";
 export const PublicRoute = () => {
   return (
-    <AnimatedSwitch
-      atEnter={{ opacity: 0 }}
-      atLeave={{ opacity: 1 }}
-      atActive={{ opacity: 1 }}
-    >
-      <Route exact path="/contacto" component={Contact} />
-      <Route exact path="/novedades" component={News} />
-      <Route path="/" component={Home} />
-    </AnimatedSwitch>
+    <>
+      <HeaderWeb />
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 1 }}
+        atActive={{ opacity: 1 }}
+      >
+        <Route exact path="/contacto" component={Contact} />
+        <Route exact path="/novedades" component={News} />
+        <Route path="/" component={Home} />
+      </AnimatedSwitch>
+      <FooterWeb />
+    </>
   );
 };

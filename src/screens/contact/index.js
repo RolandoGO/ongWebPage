7c97ea3f21489b1aa640279898eb3dd9d46f4b88
Martin Milani <martin.milani.mdq@gmail.com>
2021-07-22 23:41:29 -0300

    Integrate MapComponent on /contacto screen

--- src/screens/contact/index.js
+++ src/screens/contact/index.js
@@ -1,29 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { getContacts } from '../../services/contactService'
-import ContactData from '../../components/ContactData'
+import React, { useEffect, useState } from "react";
+import { getContacts } from "../../services/contactService";
+import ContactData from "../../components/ContactData";
+import MapComponent from "./componets/Map";
 
 function Index() {
-  
-  const [data,setData]=useState("")
-    
+  const [data, setData] = useState("");
 
-    useEffect(()=>{
-        
+  useEffect(() => {
+    getContacts().then((d) => {
+      if (d === false) setData(null);
+      else setData(d.data);
+    });
+  }, []);
 
-        getContacts().then(d=>{
-            if(d===false) setData(null)
-            else setData(d.data)
-        })
-
-
-    },[])
-
-
-    return (
-        <div>
-            <ContactData data={data}/>
-        </div>
-    )
+  return (
+    <div>
+      <MapComponent />
+      <ContactData data={data} />
+    </div>
+  );
 }
 
 export default Index;

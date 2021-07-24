51ae700e8e05dcec001af5879b0cb59e3ab03763
barbiwonderland <barbiiee.8@gmail.com>
2021-07-24 11:44:26 -0300

    adding info of user

--- src/Components/ProtectedBackoffice.js
+++ src/Components/ProtectedBackoffice.js
@@ -1,9 +1,10 @@
 import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 
 function ProtectedBackOffice({ component: Comp, ...rest }) {
-  
-  const result = auth.role_id;
+  const Auth= useSelector((state) => state.userInfo)
+  const result = Auth.user.role_id;
   if (result === 1) {
     Admin === true;
   } else {

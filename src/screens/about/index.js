6565ae92b07bb0ee16c04f08560a20b86444c728
Nico Vazquez <nicov@Nicos-MacBook-Pro.local>
2021-07-24 12:11:24 -0300

    fixed imports lowercase

--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { TitlesComponent } from "../../components/TitlesComponent";
-import Description from "./components/Description";
+import Description from "./components/description";
 import Members from "./components/members";
 
 function index() {
